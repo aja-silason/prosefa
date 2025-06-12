8be345b63354d8d2e2ab31db996f92efadd4bb71
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-06-12 19:35:08 +0100

    feat: add presentation screen

--- app/view/screens/home.tsx
+++ app/view/screens/home.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Home (){
-    return (
-        <View>
-            <Text>Text Screen</Text>
-        </View>
-    )
+    const navigate: any = useNavigation();
+    
+        const go = () => {
+            navigate.navigate("presentation");
+        }
+    
+        return (
+            <View>
+                <Text>Home Screen</Text>
+                <TouchableOpacity onPress={go}>
+                    <Text>Home ir para a Presentation</Text>
+                </TouchableOpacity>
+            </View>
+        )
 }
--- app/view/screens/presentation.tsx
+++ app/view/screens/presentation.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function PresentationScreen (){
+
+    const navigate: any = useNavigation();
+
+    const go = () => {
+        navigate.navigate("home");
+    }
 
     return (
         <View>
             <Text>Presentation Screen</Text>
+            <TouchableOpacity onPress={go}>
+                <Text>Presentation ir para a HOME</Text>
+            </TouchableOpacity>
         </View>
     )
 
